38be5eaf209fcad846cf73412eeefdc3643b7ee1
codedinsound <codedinsound@gmail.com>
2023-01-19 20:34:32 -0700

    added typescript paths and configurations for development

--- dist/index.js
+++ dist/index.js
@@ -10,10 +10,31 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.handler = void 0;
-const handler = (event) => __awaiter(void 0, void 0, void 0, function* () {
+const awsServerlessExpress = require('aws-serverless-express');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const server = awsServerlessExpress.createServer(app);
+// const enviroment = process.env.NODE_ENV || 'production'; 
+const enviroment = 'development';
+const PORT = 3000;
+const routes = express.Router({
+    mergeParams: true
+});
+routes.get('/', (req, res) => {
+    res.status(200).json({});
+});
+app.use('/user', routes);
+if (enviroment === "development") {
+    app.listen(PORT, () => console.log(`Listening on Local Port: ${PORT}`));
+}
+const handler = (event, context) => __awaiter(void 0, void 0, void 0, function* () {
     console.log(event);
+    if (enviroment === "production")
+        return awsServerlessExpress.proxy(server, event, context);
     return {
-        thing: "thing"
+        error: "No Enviroment Variable Set"
     };
 });
 exports.handler = handler;
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,41 @@
+const awsServerlessExpress = require('aws-serverless-express');
+const express = require('express'); 
+const bodyParser = require('body-parser'); 
+const cors = require('cors'); 
+const app = express(); 
+
+const server = awsServerlessExpress.createServer(app);
+
+const enviroment: String = process.env.NODE_ENV || 'production'; 
+const PORT: string = process.env.PORT || ''; 
+console.log(`Current Enviroment Set: ${enviroment}`)
+
+
+const routes = express.Router({
+    mergeParams: true
+});
+
+
+routes.get('/', (req: any, res: any) => {
+    res.status(200).json({}); 
+}); 
 
 
 
+app.use('/user', routes); 
 
-export const handler = async(event: any) => {
+if (enviroment === "development") {
+    app.listen(PORT, () => console.log(`Listening on Local Port: ${PORT}`))
+}
+
+
+export const handler = async(event: any, context: any) => {
     console.log(event);
 
+    if (enviroment === "production") 
+        return awsServerlessExpress.proxy(server, event, context); 
 
     return {
-        thing: "thing"
+        error: "No Enviroment Variable Set"
     }
 }
