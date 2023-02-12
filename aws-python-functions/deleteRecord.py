aa3ad33007dda7e20bbc03d9f04732258f5c492e
codedinsound <codedinsound@gmail.com>
2023-02-12 02:48:31 -0700

    finalized aws lambda python function ready for code touch up and refactoring

--- aws-python-functions/deleteRecord.py
+++ aws-python-functions/deleteRecord.py
@@ -8,7 +8,6 @@ from openpyxl import load_workbook
 # ===============================================================
 list_of_session_tokens = {'abc1', 'abc2', 'abc3', 'abc4', 'abc5'}
 
-
 class Database(abc.ABC):
     @abc.abstractclassmethod
     def connect(self):
@@ -20,7 +19,6 @@ class Database(abc.ABC):
     def query(self, q = ''):
         pass
 
-
 class DatabaseManagementController:
     def __init__(self):
         self.database = None
@@ -29,10 +27,10 @@ class DatabaseManagementController:
         self.database.connect()
     
     def disconnect(self):
-        print("disconnecting from database")
+        del self.database
     
     def query(self, q = ''): 
-        self.database.query(q)
+        return self.database.query(q)
 
     def setDatabase(self, database = Database):
         self.database = database
@@ -47,44 +45,59 @@ class ExcelSheetTestDatabase(Database):
         self.s3 = boto3.client('s3')
         self.workbook = None 
         self.sheet_names = []
-        self.current_sheet = ''
+        self.current_sheet = None
+
+    def __del__(self):
+        print("destroying object....")
 
     # Private Methods 
-    def __search():
-        pass 
-
-    def __delete():
-        pass 
+    def __search(self, record_id = ''):
+        print(record_id)
+        record_index = 0
+        res = {'status': 'failed', 'response': {}}
+        for row in self.current_sheet.rows:
+            if (row[0].value == record_id):
+                res['status'] = 'success'
+                res['response'] = {
+                    'record_index': record_index,
+                    'record': row
+                }
+                return res
+            record_index += 1
+        return res 
 
     def __set_worksheet(self, sheet_selected = ''): 
-        res = {'status': 'success', 'response': ''}
+        res = {'status': 'success', 'response': 'selected sheet set'}
         if (sheet_selected in self.sheet_names):
-            self.current_sheet = sheet_selected
+            self.current_sheet = self.workbook[sheet_selected]
         else:
             res = {'status': 'failed', 'response': 'could not set sheet'}
         return res 
 
-
-
+    def __update_workbook(self, update=''):
+        print("updating workbook") 
+        file_to_upload = io.BytesIO()
+        self.workbook.save(file_to_upload)
+        file_to_upload.seek(0)
+        self.s3.upload_fileobj(file_to_upload, "loanpro-challenge-aws-la-serverlessdeploymentbuck-1mo1bm3wp9e2s", "test-data/test3.xlsx")
+            
     # Public Methods 
     def connect(self):
         response = self.s3.get_object(Bucket="loanpro-challenge-aws-la-serverlessdeploymentbuck-1mo1bm3wp9e2s", Key="test-data/users_db.xlsx")
         file_stream = response['Body']
         self.workbook = load_workbook(filename = io.BytesIO(file_stream.read()))
         self.sheet_names = self.workbook.sheetnames
-        print(self.sheet_names)
 
     def disconnect(self):
-        pass
-
+        print("disconnecting from db....")
+        
     def query(self, q = ''):
-        db_res = {'status': '', 'response': None}
+        db_res = {'status': '', 'response': {}}
         queries = q.split()
         db_function_calls = {
-            'SET_WORKSHEET': self.__set_worksheet,
-            'DELETE': self.__delete,
             'SEARCH': self.__search,
-
+            'SET_WORKSHEET': self.__set_worksheet,
+            'UPDATE_WB': self.__update_workbook
         }
         if (len(queries) > 0 and queries[0] in db_function_calls):
             db_res = db_function_calls.get(queries[0])(queries[1])
@@ -92,46 +105,14 @@ class ExcelSheetTestDatabase(Database):
             db_res = {'status': 'failed', 'response': 'invalid query...'}
         return db_res 
 
-
-
-
-
-
-
-
-# ===============================================================
-
-# # # object_content = response["Body"].read()
-# # # print("Object content: %s" % object_content)
-
-# file_stream = response['Body']; 
-# wb = load_workbook(filename = io.BytesIO(file_stream.read()))
-
-# sheet_name = wb.sheetnames
-
-# ws2 = wb[sheet_name[2]] 
-
-# print(ws2)
-
-
-# mark_4_1_for_deletion = "4-6"
-# set_flag = "T"
-
-
-# for row in ws2.rows:
-#     print(row[0].value)
-#     if (row[0].value == mark_4_1_for_deletion):
-#         row[7].value = set_flag
-
-
-# wb.save("db_clone.xlsx")
-
-
 # MARK: Handle client deletion of record 
 def soft_delete_transaction_record(clientResponse, db = Database):
     print("Soft Deleting Record")
     db.query('SET_WORKSHEET UsersOperationsRecords')
-    db.query()
+    res = db.query('SEARCH ' + str(clientResponse['record']['id']))
+    if (res['status'] == 'success'):
+        res['response']['record'][7].value = 'T'
+        db.query('UPDATE_WB _')
 
 
 # MARK: Check if client session token is valid
@@ -152,13 +133,13 @@ def confirm_valid_client_reponse(clientResponse):
 def lambda_handler(event, context):
     proceed = False 
     proceed = confirm_valid_client_reponse(event)
-    print(proceed)
-    # if (proceed): 
-    #     db = DatabaseManagementController.instance().setDatabase(ExcelSheetTestDatabase())
-    #     db.connect()
-    #     soft_delete_transaction_record(event, db)
+    if (proceed): 
+        db = DatabaseManagementController.instance().setDatabase(ExcelSheetTestDatabase())
+        db.connect()
+        soft_delete_transaction_record(event, db)
+        db.disconnect()
     
 
-x = {'userID': -1, 'sessionToken': 'abc1', 'operation': 'DELETE_RECORD', 'record': {'id': '', 'operation_id': '', 'date': '2023-02-12T03:57:31.986Z', 'amount': -1, 'user_balance': -1}}
+x = {'userID': 4, 'sessionToken': 'abc1', 'operation': 'DELETE_RECORD', 'record': {'id': '4-1', 'operation_id': 'INIT', 'date': '2023-02-12T03:57:31.986Z', 'amount': -1, 'user_balance': -1}}
 lambda_handler(x, '')
 
